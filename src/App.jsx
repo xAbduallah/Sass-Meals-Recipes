5854c24f6e2147ec218493b198aa025da49b7e62
xAbduallah <xconquerps@gmail.com>
2025-02-04 10:33:33 +0200

    Auto commit on 2025-02-04 10:33:33

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,4 @@
 import Home from './Components/Home/Home'
-import './Components/Home/Home.module.css';
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import MealDetails from './Components/MealDetails';
 import LeftMenu from './Components/LeftMenu';
