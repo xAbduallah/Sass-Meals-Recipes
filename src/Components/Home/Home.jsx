d2585629bb4f00d494ed3db679560828f09468ee
xAbduallah <xconquerps@gmail.com>
2025-02-04 10:32:04 +0200

    Auto commit on 2025-02-04 10:32:04

--- src/Components/Home/Home.jsx
+++ src/Components/Home/Home.jsx
@@ -1,5 +1,4 @@
 import React, { useEffect, useState } from 'react'
-import './Home.module.css'
 import { useNavigate } from "react-router-dom";
 import axios from 'axios';
 import LeftMenu from '../LeftMenu';
--- src/Components/MealDetails.jsx
+++ src/Components/MealDetails.jsx
@@ -1,8 +1,6 @@
 import React, { useEffect, useState } from 'react';
 import { useParams } from 'react-router-dom';
 import axios from 'axios';
-import './Home/Home.module.css'
-
 
 const MealDetails = () => {
     const { mealID } = useParams();
